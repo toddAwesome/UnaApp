e55eac0ba0a3d5bb2314bf5624e9cc212824ffd6
Asigi <arshsingh_sc@yahoo.com>
2015-08-17 09:41:50 -0700

    Just started this new Una project

---
+++ app/src/main/java/com/arshsingh93/unaapp/UnaApplication.java
@@ -0,0 +1,18 @@
+package com.arshsingh93.unaapp;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+/**
+ * Created by Student on 8/17/2015.
+ */
+public class UnaApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "ZKorAmUCQmL03WosweB6tJNQXSwedHT1tPVbC6BB", "U6H8f6cbLGB2UDjLl9z6RLc3MjWVfpHWbUa6gH3r");
+    }
+
+}
