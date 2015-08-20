d0483aa7e863acb4c06db3e6dcbe3be1c4c7c22d
Todd Robbins <toddrobb11@gmail.com>
2015-08-20 06:11:51 -0700

    this is the final project

--- app/src/main/java/com/arshsingh93/unaapp/EnterActivity.java
+++ app/src/main/java/com/arshsingh93/unaapp/EnterActivity.java
@@ -41,6 +41,7 @@ public class EnterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.RedTheme);
         setContentView(R.layout.activity_enter);
 
 
